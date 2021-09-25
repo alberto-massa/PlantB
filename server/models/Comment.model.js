bfaf2a5c26363b722b42549ab41cda4fbcbee315
Alberto Massa <massa.alberto@outlook.com>
2021-09-25 13:12:42 +0200

    base folders done

---
+++ server/models/Comment.model.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+
+    plantRef: {
+      type: Number,
+    },
+
+    rating: {
+      type: Number,
+      min: 0,
+      max: 5,
+    },
+
+    authorId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    isValidated: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Comment = model("Comment", commentSchema);
+
+module.exports = Comment;
---
+++ server/models/User.model.js
@@ -0,0 +1,65 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  name: {
+    unique: true,
+    type: String,
+    required: true,
+    minlength: 4,
+    maxlength: 20,
+    trim: true,
+    set: (value) =>
+      value.charAt(0).toUpperCase() + value.substring(1).toLowerCase(),
+    match: [/^[a-zA-Z0-9]+$/, "is invalid"],
+  },
+
+  password: {
+    type: String,
+    minlength: 4,
+    maxlength: 20,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "is invalid"],
+    lowercase: true,
+    required: true,
+    trim: true
+  },
+
+  age: {
+    type: Number,
+    min: 1,
+    max: 10,
+    match: /[0-6]{3}[A-C]/,
+  },
+
+  role: {
+    type: String,
+    default: "User",
+    required: true,
+    enum: ["Guest", "User", "Admin"],
+  },
+
+  avatar: {
+    type: String,
+    default: "to-do.png"
+  },
+
+  favouritePlants: {
+    type: [Number],
+    unique: true
+  },
+
+  favouriteSeller: {
+    type: [Number],
+    unique: true
+  }
+
+},{timestamps: true});
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User;
