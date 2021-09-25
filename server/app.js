fbebdf355679bb4c069551c4dcaf14acda2de80d
jose ramon <rabohopo@gmail.com>
2021-09-25 16:40:07 +0200

    add and index

--- server/app.js
+++ server/app.js
@@ -15,20 +15,16 @@ const hbs = require("hbs");
 
 const app = express();
 
-// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
-// default value for title local
 const projectName = "PlantB";
 const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
 
-// 👇 Start handling routes here
 const index = require("./routes/index");
 app.use("/", index);
 
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
 module.exports = app;
